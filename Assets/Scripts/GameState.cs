7b2d66300ab66d8518f5e54abf86685754e5c91b
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2023-12-13 18:08:21 -0500

    added game state and game logic

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "gameState", menuName = "State/DuiDashGameState")]
+public class GameState : ScriptableObject
+{
+    public string playerName { get; set; }
+    public float bacPercent { get; set; }
+    public float scoreMultiplier { get; set; }
+    public float score { get; set; }
+}
