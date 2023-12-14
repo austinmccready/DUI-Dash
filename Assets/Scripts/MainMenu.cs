cc3dd7ef47c37809589d752af2e73b04e9ec0474
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2023-12-13 20:01:36 -0500

    user can add name; saved to GameState

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement; // to change scenes in unity
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    TMP_InputField playerNameField;
+
     public void PlayGame()
     {
+        SetPlayerName(playerNameField.text);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetPlayerName(string name)
+    {
+        GameState gameState = ScriptableObject.CreateInstance<GameState>();
+        gameState.playerName = name;
+        Debug.Log("Player name is " + name);
     }
 }
